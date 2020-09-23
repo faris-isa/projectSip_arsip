8d8419fd4203eb3eaefade008e048068daeabaf9
Faris Isa <m.faris.isa@gmail.com>
2020-09-23 19:35:14 +0700

    fixxing route resource products

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', ProductsController::class); 
+// Route::get('products', 'ProductController@index');
+Route::resource('products', ProductController::class); 
